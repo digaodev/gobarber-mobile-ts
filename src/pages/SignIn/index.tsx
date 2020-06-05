acb1dce10818a309505ab3e225e58db82051be25
digaodev <rodrigo.rb.dev@gmail.com>
2020-06-05 17:03:51 -0300

    feat: add external font; Add logo image and png types.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container style={{ flex: 1 }}>
-      <Text>SignIn page</Text>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
